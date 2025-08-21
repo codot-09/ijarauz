771a007d1d7a8d4adfd52f78ecb5dddc3e8b0979
Quvonch09 <quvonchbekbobomurodov9@gmail.com>
2025-08-21 17:21:55 +0500

    Pdf yuklash va cloud tugadi

---
+++ src/main/java/com/example/ijara/controller/FileController.java
@@ -0,0 +1,31 @@
+package com.example.ijara.controller;
+
+import com.example.ijara.service.impl.CloudService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api/v1/files")
+@RequiredArgsConstructor
+@Tag(name = "Files", description = "Filelar bilan ishlash")
+public class FileController {
+    private final CloudService cloudService;
+
+    @PostMapping(value = "/upload",consumes = "multipart/form-data")
+    @Operation(summary = "Rasm yuklash")
+    public ResponseEntity<String> uploadFile(
+            @RequestParam("file") MultipartFile file
+    ) throws IOException {
+        String url = cloudService.uploadFile(file);
+        return ResponseEntity.ok(url);
+    }
+}
